572e3e4059720a1fe9b6f06b4ef4071f5cf246f6
jahnvisrivastava100 <srivastavajahnvi6@>
2021-07-26 10:58:24 +0530

    navbar font change

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ if ('speechSynthesis' in window) {
      alert("Sorry, your browser doesn't support text to speech!");
    }
 var msg = new SpeechSynthesisUtterance();
-msg.rate = 0.8;
+
 msg.text = " Our universe began with an explosion of space itself - the Big Bang. Starting from extremely high density and temperature, space expanded, the universe cooled, and the simplest elements formed. Gravity gradually drew matter together to form the first stars and the first galaxies. Galaxies collected into groups, clusters, and superclusters.    Some stars died in supernova explosions, whose chemical remnants seeded new generations of stars and enabled the formation of rocky planets. On at least one such planet, life evolved to consciousness.    And it wonders, “Where did I come from?”";
 window.speechSynthesis.speak(msg);
 
