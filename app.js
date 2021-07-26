77c22ba02c2e9eb18e32eb4e5ff73f9507a58221
jahnvisrivastava100 <srivastavajahnvi6@>
2021-07-26 17:37:25 +0530

    JS file updated

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
+// a = {
+//     show: function(elem) {
+//         document.getElementById(elem).style.visibility = 'visible';
+//     },
+//     hide: function(elem) {
+//         document.getElementById(elem).style.visibility = 'hidden';
+//     }
+// }
+// b = {
+//     show: function(elem) {
+//         document.getElementById(elem).style.visibility = 'visible';
+//     },
+//     hide: function(elem) {
+//         document.getElementById(elem).style.visibility = 'hidden';
+//     }
+// }
+// function speak(){
+// if ('speechSynthesis' in window) {
+//     // Speech Synthesis supported 🎉
+//    }else{
+//      // Speech Synthesis Not Supported 😣
+//      alert("Sorry, your browser doesn't support text to speech!");
+//    }
+// var msg = new SpeechSynthesisUtterance();
+
+// msg.text = " Our universe began with an explosion of space itself - the Big Bang. Starting from extremely high density and temperature, space expanded, the universe cooled, and the simplest elements formed. Gravity gradually drew matter together to form the first stars and the first galaxies. Galaxies collected into groups, clusters, and superclusters.    Some stars died in supernova explosions, whose chemical remnants seeded new generations of stars and enabled the formation of rocky planets. On at least one such planet, life evolved to consciousness.    And it wonders, “Where did I come from?”";
+// window.speechSynthesis.speak(msg);
+
+// }
+
+
+
 a = {
-    show: function(elem) {
-        document.getElementById(elem).style.visibility = 'visible';
+    show: function (elem) {
+      document.getElementById(elem).style.visibility = "visible";
     },
-    hide: function(elem) {
-        document.getElementById(elem).style.visibility = 'hidden';
-    }
-}
-b = {
-    show: function(elem) {
-        document.getElementById(elem).style.visibility = 'visible';
+    hide: function (elem) {
+      document.getElementById(elem).style.visibility = "hidden";
     },
-    hide: function(elem) {
-        document.getElementById(elem).style.visibility = 'hidden';
+  };
+  b = {
+    show: function (elem) {
+      document.getElementById(elem).style.visibility = "visible";
+    },
+    hide: function (elem) {
+      document.getElementById(elem).style.visibility = "hidden";
+    },
+  };
+  function speak() {
+    if ("speechSynthesis" in window) {
+      // Speech Synthesis supported 🎉
+    } else {
+      // Speech Synthesis Not Supported 😣
+      alert("Sorry, your browser doesn't support text to speech!");
     }
-}
-function speak(){
-if ('speechSynthesis' in window) {
-    // Speech Synthesis supported 🎉
-   }else{
-     // Speech Synthesis Not Supported 😣
-     alert("Sorry, your browser doesn't support text to speech!");
-   }
-var msg = new SpeechSynthesisUtterance();
 
-msg.text = " Our universe began with an explosion of space itself - the Big Bang. Starting from extremely high density and temperature, space expanded, the universe cooled, and the simplest elements formed. Gravity gradually drew matter together to form the first stars and the first galaxies. Galaxies collected into groups, clusters, and superclusters.    Some stars died in supernova explosions, whose chemical remnants seeded new generations of stars and enabled the formation of rocky planets. On at least one such planet, life evolved to consciousness.    And it wonders, “Where did I come from?”";
-window.speechSynthesis.speak(msg);
+    // Change 1 (To restart the audio)
+    window.speechSynthesis.cancel();
 
-}
+    var msg = new SpeechSynthesisUtterance();
+    msg.text =
+      " Our universe began with an explosion of space itself - the Big Bang. Starting from extremely high density and temperature, space expanded, the universe cooled, and the simplest elements formed. Gravity gradually drew matter together to form the first stars and the first galaxies. Galaxies collected into groups, clusters, and superclusters.    Some stars died in supernova explosions, whose chemical remnants seeded new generations of stars and enabled the formation of rocky planets. On at least one such planet, life evolved to consciousness.    And it wonders, “Where did I come from?”";
+    window.speechSynthesis.speak(msg);
+  }
 
+  // Change 2 (To end sound if the page gets reloaded)
+  window.addEventListener("load", () => {
+    window.speechSynthesis.cancel();
+  });
